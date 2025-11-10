4bdb5bec5a24e55c58484b1a5d3fe50af8460b06
noygav <noygav@gmail.com>
2025-11-10 23:14:02 +0200

    finish

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
-public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+public class Cheers
+ {
+        public static void main(String[] args)
+         {
+                String name = args[0];
+                int count = Integer.parseInt(args[1]);
+                name = name.toUpperCase();
+                int length = name.length();
+
+                String rules = "AEFHILMNORSX";
+                for (int i=0; i<length; i++)
+                {
+                        System.out.print("Give me " );
+                        if( rules.indexOf(name.charAt(i)) >= 0 )
+                        { 
+                                System.out.print("an ");
+                        }
+                        else
+                        {
+                                System.out.print("a ");
+                        }       
+                         System.out.println(name.charAt(i)+ ":" + name.charAt(i) + "!");
+                }
+                System.out.println("What does that spell? ");
+                for (int j=0; j<count; j++)
+                {
+                        System.out.println(name + "!!!" );
+                }
         }
 }
